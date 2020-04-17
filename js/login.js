c36fdf158dbff84d31664a7c9bea3ab82f6f9e12
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-04-17 18:44:06 -0500

    Se permite el ingreso de empleado o empresa en el login

--- js/login.js
+++ js/login.js
@@ -6,24 +6,21 @@ firebase.initializeApp({
 
 const db = firebase.firestore();
 
+const verificarEmpleado = (email, password) => {
+   let verificar = false;
+  db.collection("empleados").onSnapshot((querySnapshot) => {
 
-
-const verificar = () => {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  let verificar = false;
-  db.collection("users").onSnapshot((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      if (email == doc.data().email && password == doc.data().contraseña) {    
-        
-        localStorage.setItem('Nombre', doc.data().nomEmpresa);
+     
+      if (email == doc.data().name && password == doc.data().contraseña) {
+
+        localStorage.setItem('Nombre', doc.data().name);
         localStorage.setItem('Id', doc.id);
         // limpia los inputs
         document.getElementById("email").value = "";
         document.getElementById("password").value = "";
-        window.location = "html/empleados.html";
+        window.location = "html/preguntas.html";
         verificar = true;
-        
       }
 
     });
@@ -33,48 +30,87 @@ const verificar = () => {
         icon: 'error',
         title: 'Email o Contraseña incorrecta',
         text: 'Intenta de nuevo',
-       
+
       })
     }
-    
+
   });
-
-
 }
 
-const vedddrificar = () =>{
+const verificarEmpresa = (email, password) => {
+  let verificar = false;
+  db.collection("users").onSnapshot((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      if (email == doc.data().email && password == doc.data().contraseña) {
+        localStorage.setItem('Nombre', doc.data().nomEmpresa);
+        localStorage.setItem('Id', doc.id);
+        // limpia los inputs
+        document.getElementById("email").value = "";
+        document.getElementById("password").value = "";
+        window.location = "html/empleados.html";
+        verificar = true;
+
+      }
+
+    });
+
+    if (!verificar) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Email o Contraseña incorrecta',
+        text: 'Intenta de nuevo',
+
+      })
+    }
+
+  });
+}
+
+const verificar = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const empresa = document.querySelector("#gridRadios1").checked;
+  if(empresa == true){
+    verificarEmpresa(email, password);
+    
+  }else{
+    verificarEmpleado(email, password);
+  }
+}
+
+const vedddrificar = () => {
   const username = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  
 
-  firebase.auth().signInWithEmailAndPassword(username, password).catch(function(error) {
+
+  firebase.auth().signInWithEmailAndPassword(username, password).catch(function (error) {
     var errorCode = error.code;
     var errorMessage = error.message;
 
-});
-firebase.auth().onAuthStateChanged(function(user) {
+  });
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
-        firebase.auth().currentUser.getIdToken().then(function(idToken) {
-            localStorage.auth = idToken;
-            localStorage.uid = firebase.auth().currentUser.uid;
+      firebase.auth().currentUser.getIdToken().then(function (idToken) {
+        localStorage.auth = idToken;
+        localStorage.uid = firebase.auth().currentUser.uid;
 
-            uid = firebase.auth().currentUser.uid;
-            firebase.database().ref("users/" + uid).update({
-                "name": $("#nameUser").val(),
-                "status": "0",
-                "uid": uid,
-                "challenge": false,
-                "statusChallenge": false
-            });
+        uid = firebase.auth().currentUser.uid;
+        firebase.database().ref("users/" + uid).update({
+          "name": $("#nameUser").val(),
+          "status": "0",
+          "uid": uid,
+          "challenge": false,
+          "statusChallenge": false
         });
-        window.location = "html/empleados.html";
+      });
+      window.location = "html/empleados.html";
     } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Usuario y/o contraseña son incorrectos',
-        });
+      Swal.fire({
+        icon: 'error',
+        title: 'Usuario y/o contraseña son incorrectos',
+      });
     }
-});
+  });
 
 
 }
