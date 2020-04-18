2b70f9eeeaa1337a5fcd6e841eaf0175a3a45fe8
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-04-17 19:37:45 -0500

    Se comenzo a agregar las imagenes a firebase

--- js/registroEmpleado.js
+++ js/registroEmpleado.js
@@ -9,23 +9,42 @@ if(nom != null){
 }
 
 
-firebase.initializeApp({
+
+var config = {
     apiKey: "AIzaSyBIWplHf1UW47MdtAxRq9sSm_OdxcQiKF4",
     authDomain: "parcial-db.firebaseapp.com",
-    projectId: "parcial-db"
-});
+    projectId: "parcial-db",
+    storageBucket: "parcial-db.appspot.com",
+};
+
+firebase.initializeApp(config);
 
 const db = firebase.firestore();
+const storage = firebase.storage();
 
 const obtenerDatos = () => {
     const contraseña = document.getElementById("passwordEmpleado").value;
     const name = document.getElementById("nameEmpleado").value;
     const direccion = document.getElementById("direccion").value;
+    const file = document.getElementById("file").files[0];
+    console.log(file)
+    if(!file){
 
+    }else{
+        const storageRef = storage.ref('empleados/img/' + file.name);
+        const upload = storageRef.put(file);
+        upload.on('state_changed', (snapshot)=>{
+
+        }, function(error){
+            console.log("error")
+        }, function(){
+            console.log("Subido a firebase")
+        });
+    }
+    
 
     if (direccion.length == 0 || contraseña.length == 0 || name.length == 0) {
         
-
         Swal.fire({
             title: 'Informacion incompleta',
             text: "Debe llenar todos los campos",
