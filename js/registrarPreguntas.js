eecf07ba6d8202ad01ec11678e59d4b793424814
Juan Carlos Hoyos <jcabarique@uninorte.edu.co>
2020-04-21 18:54:05 -0500

    Se modifico el registro de preguntas | se modifico el Json

--- js/registrarPreguntas.js
+++ js/registrarPreguntas.js
@@ -7,18 +7,11 @@ const crearPreguntas = () => {
     const pregunta3 = document.getElementById("pregunta3").value;
     const pregunta4 = document.getElementById("pregunta4").value;
     const pregunta5 = document.getElementById("pregunta5").value;
-
-    const valor1 = document.getElementById("valor1").value;
-    const valor2 = document.getElementById("valor2").value;
-    const valor3 = document.getElementById("valor3").value;
-    const valor4 = document.getElementById("valor4").value;
-    const valor5 = document.getElementById("valor5").value;
     const db = firebase.firestore();
     
 
     if (pregunta1.length == 0 || pregunta2.length == 0 || pregunta3.length == 0 || pregunta4.length == 0 ||
-        pregunta5.length == 0 || valor1.length == 0 || valor2.length == 0 || valor3.length == 0 || valor4.length == 0
-        || valor5.length == 0) {
+        pregunta5.length == 0 ) {
 
         Swal.fire({
             title: 'Informacion incompleta',
@@ -28,11 +21,9 @@ const crearPreguntas = () => {
         })
     } else {
         const uid = localStorage.getItem('IdEmpresa')
-        const data = array(pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, 
-            valor1, valor2, valor3, valor4, valor5, uid);
+        const data = array(pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, uid);
         let verificar = false;
 
-        console.log(uid);
         db.collection("Preguntas").onSnapshot((querySnapshot) => {
             querySnapshot.forEach((doc) => {     
                 console.log(doc.data().uidEmpresa);
@@ -78,19 +69,13 @@ const crearPreguntas = () => {
     }
 }
 
-const array = (pregunta1, pregunta2, pregunta3, pregunta4, pregunta5, 
-    valor1, valor2, valor3, valor4, valor5, uid) => {
+const array = (pregunta1, pregunta2, pregunta3, pregunta4, pregunta5,uid) => {
     const data = {
         pregunta1: pregunta1,
         pregunta2:pregunta2,
         pregunta3: pregunta3,
         pregunta4: pregunta4,
         pregunta5: pregunta5,
-        valor1: valor1,
-        valor2: valor2,
-        valor3: valor3,
-        valor4: valor4,
-        valor5: valor5,
         uidEmpresa: uid
     }
     return data;
