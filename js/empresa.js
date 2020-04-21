2914f16bcddf86ce27c1895625bc12f342e6e853
Abraham Lugo Ramirez <labrahamdedios@gmail.com>
2020-04-21 13:18:57 -0500

    information

--- js/empresa.js
+++ js/empresa.js
@@ -7,7 +7,7 @@ var cont = 0;
 document.getElementById("img").src = localStorage.getItem('Url');
 document.getElementById("nombre-empresa").innerHTML = localStorage.getItem('Nombre');
 document.getElementById("imgen").src = localStorage.getItem('Url');
-document.getElementById("nombre-empres").innerHTML = localStorage.getItem('Nombre');
+document.getElementById("nombre-empres").innerHTML = "Nombre empresa: " + localStorage.getItem('Nombre');
 db.collection("empleados").onSnapshot((querySnapshot) => {
     const uidEmpresa = localStorage.getItem('IdEmpresa');
     var a = document.getElementById("lista");
@@ -17,12 +17,21 @@ db.collection("empleados").onSnapshot((querySnapshot) => {
             cont = cont +1;
         }        
     });
+    db.collection("users").onSnapshot((querySnapshot) => {
+        const uidEmpresa = localStorage.getItem('IdEmpresa');               
+        querySnapshot.forEach((doc) => {
+            if (uidEmpresa == doc.data().uid) {
+                document.getElementById("email").innerHTML = "Email: " + doc.data().email;
+                              
+
+            }      
+
+
+        });
+    
+    
+    });
     console.log(cont);
-
-
-
-
-
-
+    document.getElementById("numero").innerHTML = "Numero de empleados: " + cont;
 
 });
